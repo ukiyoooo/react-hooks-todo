02eb20005bc9daac97b882c1c522820fe9758341
ukiyoooo <13175394+ukiyoooo@users.noreply.github.com>
2020-06-27 02:27:38 +0900

    Use other Bulma components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Form = (props) => (
-    <form className="box" onSubmit={props.addTodo}>
-        <div className="field">
-            <label className="label is-large">
-                Your todo.
-            </label>
-            <input name="title" type="text" className="input" placeholder="What's your new task?" />
-            <input type="submit" value="Add" className="button is-primary" />
-        </div>
-    </form>
+    <div className="container">
+        <form className="box" onSubmit={props.addTodo}>
+            <div className="field">
+                <label className="label is-large">
+                    Add a task
+                </label>
+                <input name="title" type="text" className="input" placeholder="What's your new task?" />
+                <input type="submit" value="Add" className="button is-primary" />
+            </div>
+        </form>
+    </div>
 );
 
 export default Form;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -9,15 +9,21 @@ let btn = {
 };
 
 const List = (props) => (
-    <div className="content">
+    <div className="container">
         <ul>
             {
                 props.todoList.map((todo, i) => (
-                    <li key={i} className="" style={style}>{todo.title} <span className="tag is-danger" style={btn} onClick={() => props.removeTodo(todo.id)}>Delete</span></li>)
-                )
+                    <div key={i}>
+                        <div className="box">
+                            <label className="checkbox">
+                                <input type="checkbox" />
+                                {todo.title} <span className="tag is-danger" style={btn} onClick={() => props.removeTodo(todo.id)}>Delete</span>
+                            </label>
+                        </div>
+                    </div>
+                ))
             }
         </ul>
-
     </div>
 );
 
