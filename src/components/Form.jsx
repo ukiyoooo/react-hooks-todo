ada294ae2cbc2b1301125aeff119181829e33585
ukiyoooo <13175394+ukiyoooo@users.noreply.github.com>
2020-06-27 13:18:35 +0900

    Modified submission field layouts

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,12 +3,16 @@ import React from "react";
 const Form = (props) => (
     <div className="container">
         <form className="box" onSubmit={props.addTodo}>
-            <div className="field">
-                <label className="label is-large">
-                    Add a task
-                </label>
-                <input name="title" type="text" className="input" placeholder="What's your new task?" />
-                <input type="submit" value="Add" className="button is-primary" />
+            <label className="label is-large">
+                Add a task
+            </label>
+            <div className="field has-addons">
+                <div className="control is-expanded">
+                    <input name="title" type="text" className="input" placeholder="What's your new task?" />
+                </div>
+                <div className="control">
+                    <input type="submit" value="Add" className="button is-info" />
+                </div>
             </div>
         </form>
     </div>
