d7fa12b0fcd21b851e617fbd951472024fb4bb96
ukiyoooo <13175394+ukiyoooo@users.noreply.github.com>
2020-06-29 21:26:20 +0900

    refactoring

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,37 @@
-import React, { ChangeEvent, FormEvent } from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 
 interface Props {
-    inputText: string,
-    handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
-    handleSubmit: (event: FormEvent<HTMLFormElement>) => void;
+    addTodo: (event: FormEvent<HTMLFormElement>, title: string) => void;
 }
 
-const Form: React.FC<Props> = (props) => (
-    <div className="container">
-        <form className="box" onSubmit={props.handleSubmit}>
-            <label className="label is-large">
-                Add a task
+const Form: React.FC<Props> = (props) => {
+    const [inputValue, setInputValue] = useState('');
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        props.addTodo(event, inputValue);
+        setInputValue('');
+    };
+
+    return (
+        <div className="container">
+            <form className="box" onSubmit={handleSubmit}>
+                <label className="label is-large">
+                    Add a task
             </label>
-            <div className="field has-addons">
-                <div className="control is-expanded">
-                    <input name="title" type="text" className="input" placeholder="What's your new task?" onChange={props.handleChange} value={props.inputText} />
+                <div className="field has-addons">
+                    <div className="control is-expanded">
+                        <input name="title" type="text" className="input" placeholder="What's your new task?" onChange={handleChange} value={inputValue} />
+                    </div>
+                    <div className="control">
+                        <input type="submit" value="Add" className="button is-info" />
+                    </div>
                 </div>
-                <div className="control">
-                    <input type="submit" value="Add" className="button is-info" />
-                </div>
-            </div>
-        </form>
-    </div>
-);
+            </form>
+        </div>
+    );
+}
 
 export default Form;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, ChangeEvent, FormEvent } from "react";
+import React, { useReducer, FormEvent } from "react";
 import Form from "./Form";
 import List from "./List";
 import { generateUniqueID, dateToFormatString } from "../util";
@@ -12,12 +12,10 @@ export interface Todo {
 export type TodoList = Todo[];
 
 interface TodoState {
-    currentInput: string,
     todoList: TodoList
 }
 
 enum TodoActionType {
-    ACTION_INPUT = 'ACTION_INPUT',
     ACTION_ADD = 'ACTION_ADD',
     ACTION_REMOVE = 'ACTION_REMOVE',
 }
@@ -29,19 +27,12 @@ interface TodoAction {
 
 const todoReducer: React.Reducer<TodoState, TodoAction> = (state: TodoState, action: TodoAction) => {
     switch (action.type) {
-        case TodoActionType.ACTION_INPUT:
-            return {
-                currentInput: action.payload.title,
-                todoList: state.todoList
-            };
         case TodoActionType.ACTION_ADD:
             return {
-                currentInput: "",
                 todoList: [...(state.todoList), action.payload]
             };
         case TodoActionType.ACTION_REMOVE:
             return {
-                currentInput: state.currentInput,
                 todoList: state.todoList.filter((todo: Todo) => todo.id !== action.payload.id)
             };
         default: {
@@ -51,7 +42,7 @@ const todoReducer: React.Reducer<TodoState, TodoAction> = (state: TodoState, act
 }
 
 const Main: React.FC = () => {
-    const initialState: TodoState = { currentInput: "", todoList: [] };
+    const initialState: TodoState = { todoList: [] };
     let [todoState, todoDispatch] = useReducer(todoReducer, initialState);
 
     return (
@@ -62,30 +53,21 @@ const Main: React.FC = () => {
             <div className="column">
                 <section className="section">
                     <Form
-                        inputText={todoState.currentInput}
-                        handleChange={(event: ChangeEvent<HTMLInputElement>) => {
-                            todoDispatch({
-                                type: TodoActionType.ACTION_INPUT,
-                                payload: {
-                                    id: "",
-                                    title: event.target.value,
-                                    createdAt: ""
-                                },
-                            });
-                        }}
-                        handleSubmit={(event: FormEvent<HTMLFormElement>) => {
-                            event.preventDefault();
-                            if (todoState.currentInput !== "") {
-                                todoDispatch({
-                                    type: TodoActionType.ACTION_ADD,
-                                    payload: {
-                                        id: generateUniqueID(),
-                                        title: todoState.currentInput,
-                                        createdAt: dateToFormatString(new Date(), '%YYYY%-%MM%-%DD%-%HH%-%mm%-%ss%')
-                                    },
-                                });
+                        addTodo={
+                            (event: FormEvent<HTMLFormElement>, inputValue: string) => {
+                                event.preventDefault();
+                                if (inputValue !== "") {
+                                    todoDispatch({
+                                        type: TodoActionType.ACTION_ADD,
+                                        payload: {
+                                            id: generateUniqueID(),
+                                            title: inputValue,
+                                            createdAt: dateToFormatString(new Date(), '%YYYY%-%MM%-%DD%-%HH%-%mm%-%ss%')
+                                        },
+                                    });
+                                }
                             }
-                        }}
+                        }
                     />
                     <List
                         todoList={todoState.todoList}
